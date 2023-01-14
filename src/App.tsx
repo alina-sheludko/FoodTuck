dc41d89458b31fc28208338bd817caf062f7e8ca
alina-sheludko <86827715+alina-sheludko@users.noreply.github.com>
2023-01-14 15:48:23 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss';
+import Header from './main-layout-components/header/Header';
+import HomePage from './pages/home/HomePage';
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <HomePage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/main-layout-components/header/Header.tsx
@@ -0,0 +1,12 @@
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <p>Hello World!!!</p>
+
+    </header>
+  )
+}
+
+export default Header;
