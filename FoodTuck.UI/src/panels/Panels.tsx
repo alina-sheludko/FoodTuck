bc47294905c18f3f6125b3ed3b77115062c6599d
alina-sheludko <alinasheludko.work@gmail.com>
2023-05-07 22:44:16 +0300

    panels resolver

---
+++ FoodTuck.UI/src/panels/Panels.tsx
@@ -0,0 +1,44 @@
+import FAQPanel from "./faq/FAQPanel";
+import OurTeamPanel from './ourTeam/OurTeamPanel';
+import HomeHeroPanel from '../panels/homeHero/HomeHeroPanel';
+import IconsPanel from "./icons/IconsPanel";
+import ClientsPanel from "./clients/ClientsPanel";
+import ImagePanel from "./image/ImagePanel";
+import VideoPanel from './video/VideoPanel';
+
+interface IProps {
+  panels: IPanel[]
+}
+
+interface IPanel {
+  panelAlias: string;
+}
+
+const Panels = ({panels}: IProps) => {
+  return (
+    <main>
+      {
+        panels.map((el: any) => {
+          switch (el.panelAlias){
+            case "faqPanel":
+              return <FAQPanel data={el} key={el.id}/>
+            case 'ourTeamPanel':
+              return <OurTeamPanel data={el} key={el.id}/>
+            case 'homeHeroPanel':
+              return <HomeHeroPanel data={el} key={el.id}/>
+            case 'iconsPanel':
+              return <IconsPanel data={el} key={el.id}/>
+            case 'iconsExtendedPanel':
+              return <ClientsPanel data={el} key={el.id}/>
+            case 'imagePanel':
+              return <ImagePanel data={el} key={el.id}/>
+            case 'videoPanel':
+            return <VideoPanel data={el} key={el.id}/>
+          }
+        })        
+      }
+    </main>
+  )
+}
+
+export default Panels;
