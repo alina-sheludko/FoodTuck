d8e3949a0b32c389b82f0bc55b1dcd2c7cd79f28
alina-sheludko <alinasheludko.work@gmail.com>
2023-05-07 23:17:59 +0300

    our team page

---
+++ FoodTuck.UI/src/pages/ourTeam/OurTeamPage.tsx
@@ -0,0 +1,41 @@
+import styles from './OurTeamPage.module.scss';
+import HeroPanel from '../../panels/hero/HeroPanel';
+import layoutStyles from '../../styles/layout.module.scss';
+import Picture, { IImage } from '../../shared/components/picture/Picture';
+
+interface IProps {
+  pageData: IPageData
+}
+
+interface IPageData {
+  teamMembers: ITeamMembers[]
+}
+
+interface ITeamMembers {
+  name: string,
+  surname: string,
+  jobTitle: string,
+  img: IImage
+}
+
+const OurTeamPage = ({pageData}: IProps) => {
+  return (
+    <> 
+      <HeroPanel/>
+      
+      <div className={`${styles.ourTeamPage} ${layoutStyles.container}`}>
+        {pageData.teamMembers.map((el, i) => (
+          <div className={styles.memberCard} key={i}>
+            <Picture data={{...el.img, alt: `${el.name[0]}. ${el.surname}`}} classForImg={`${styles.memberCardImg}`}/>
+            
+            <p className={styles.memberCardName}>{el.name} {el.surname}</p>
+            
+            <p className={styles.memberCardJob}>{el.jobTitle}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default OurTeamPage;
