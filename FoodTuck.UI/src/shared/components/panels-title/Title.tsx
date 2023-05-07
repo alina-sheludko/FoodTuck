06d0e2ebc24b5210c272e58f18b370c8047202b5
alina-sheludko <alinasheludko.work@gmail.com>
2023-05-07 23:08:29 +0300

    added panels title

---
+++ FoodTuck.UI/src/shared/components/panels-title/Title.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import styles from './Title.module.scss';
+
+interface IProps {
+  title: string,
+  className: string
+}
+
+const Title = ({title, className}: IProps) => {
+  const [highlightedTitle, setHighlightedTitle] = useState('');
+  const [mainTitle, setMainTitle] = useState('');
+
+  if(title) {
+    useEffect(() => {
+    setHighlightedTitle(title.slice(0, 2));
+    setMainTitle(title.slice(2));
+    }, [title]);
+  }
+
+  return (
+    <>
+      {
+        title &&
+        <h2 className={`${className} ${styles.title}`}>
+          <span className={styles.titleHighlightedText}>{highlightedTitle}</span>
+          {mainTitle}
+        </h2>
+      }
+    </>
+  )
+} 
+
+export default Title;
